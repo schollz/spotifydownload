ec4c319c99009d77ab98d06a0f64cc5b302a84f6
Zack Scholl <zack.scholl@gmail.com>
2019-01-12 12:53:59 -0800

    switch to Go

---
+++ main.go
@@ -0,0 +1,212 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/schollz/getsong"
+)
+
+type Spotify struct {
+	Href  string `json:"href"`
+	Items []struct {
+		AddedAt time.Time `json:"added_at"`
+		AddedBy struct {
+			ExternalUrls struct {
+				Spotify string `json:"spotify"`
+			} `json:"external_urls"`
+			Href string `json:"href"`
+			ID   string `json:"id"`
+			Type string `json:"type"`
+			URI  string `json:"uri"`
+		} `json:"added_by"`
+		IsLocal      bool         `json:"is_local"`
+		PrimaryColor *interface{} `json:"primary_color"`
+		Track        struct {
+			Album struct {
+				AlbumType string `json:"album_type"`
+				Artists   []struct {
+					ExternalUrls struct {
+						Spotify string `json:"spotify"`
+					} `json:"external_urls"`
+					Href string `json:"href"`
+					ID   string `json:"id"`
+					Name string `json:"name"`
+					Type string `json:"type"`
+					URI  string `json:"uri"`
+				} `json:"artists"`
+				AvailableMarkets []string `json:"available_markets"`
+				ExternalUrls     struct {
+					Spotify string `json:"spotify"`
+				} `json:"external_urls"`
+				Href   string `json:"href"`
+				ID     string `json:"id"`
+				Images []struct {
+					Height int    `json:"height"`
+					URL    string `json:"url"`
+					Width  int    `json:"width"`
+				} `json:"images"`
+				Name                 string `json:"name"`
+				ReleaseDate          string `json:"release_date"`
+				ReleaseDatePrecision string `json:"release_date_precision"`
+				TotalTracks          int    `json:"total_tracks"`
+				Type                 string `json:"type"`
+				URI                  string `json:"uri"`
+			} `json:"album"`
+			Artists []struct {
+				ExternalUrls struct {
+					Spotify string `json:"spotify"`
+				} `json:"external_urls"`
+				Href string `json:"href"`
+				ID   string `json:"id"`
+				Name string `json:"name"`
+				Type string `json:"type"`
+				URI  string `json:"uri"`
+			} `json:"artists"`
+			AvailableMarkets []string `json:"available_markets"`
+			DiscNumber       int      `json:"disc_number"`
+			DurationMs       int      `json:"duration_ms"`
+			Episode          bool     `json:"episode"`
+			Explicit         bool     `json:"explicit"`
+			ExternalIds      struct {
+				Isrc string `json:"isrc"`
+			} `json:"external_ids"`
+			ExternalUrls struct {
+				Spotify string `json:"spotify"`
+			} `json:"external_urls"`
+			Href        string `json:"href"`
+			ID          string `json:"id"`
+			IsLocal     bool   `json:"is_local"`
+			Name        string `json:"name"`
+			Popularity  int    `json:"popularity"`
+			PreviewURL  string `json:"preview_url"`
+			Track       bool   `json:"track"`
+			TrackNumber int    `json:"track_number"`
+			Type        string `json:"type"`
+			URI         string `json:"uri"`
+		} `json:"track"`
+		VideoThumbnail struct {
+			URL *interface{} `json:"url"`
+		} `json:"video_thumbnail"`
+	} `json:"items"`
+	Limit    int          `json:"limit"`
+	Next     *interface{} `json:"next"`
+	Offset   int          `json:"offset"`
+	Previous *interface{} `json:"previous"`
+	Total    int          `json:"total"`
+}
+
+type Result struct {
+	err   error
+	track Track
+}
+
+type Track struct {
+	title    string
+	artist   string
+	duration int
+}
+
+func main() {
+	playlistID := flag.String("playlist", "", "The Spotify playlist ID")
+	bearerToken := flag.String("bearer", "", "Bearer token to use for Spotify")
+	flag.Parse()
+	err := run(*bearerToken, *playlistID)
+	if err != nil {
+		fmt.Printf("Problem running: %s", err.Error())
+	}
+}
+
+func run(bearerToken, playlistID string) (err error) {
+	getsong.OptionShowProgressBar = false
+
+	spotifyJSON, err := getSpotifyPlaylist(bearerToken, playlistID)
+	if err != nil {
+		return
+	}
+	if len(spotifyJSON.Items) == 0 {
+		err = fmt.Errorf("found no tracks")
+		return
+	}
+
+	os.Mkdir(playlistID, 0644)
+	os.Chdir(playlistID)
+
+	workers := 30
+
+	tracksToDownload := make([]string, len(spotifyJSON.Items))
+
+	jobs := make(chan Track, len(tracksToDownload))
+
+	results := make(chan Result, len(tracksToDownload))
+
+	for w := 0; w < workers; w++ {
+		go func(jobs <-chan Track, results chan<- Result) {
+			for j := range jobs {
+				var err error
+
+				id, err := getsong.GetMusicVideoID(j.title+" "+j.artist, j.duration)
+				if err == nil {
+					fmt.Printf("Downloading %s by %s...\n", j.title, j.artist)
+					fname, err := getsong.DownloadYouTube(id, j.artist+" - "+j.title)
+					if err == nil {
+						fmt.Printf("Converting %s by %s...\n", j.title, j.artist)
+						err = getsong.ConvertToMp3(fname)
+						if err == nil {
+							fmt.Printf("Finished %s by %s...\n", j.title, j.artist)
+						}
+					}
+				}
+
+				results <- Result{
+					track: j,
+					err:   err,
+				}
+			}
+		}(jobs, results)
+	}
+
+	for _, song := range spotifyJSON.Items {
+		jobs <- Track{
+			title:    song.Track.Name,
+			artist:   song.Track.Artists[0].Name,
+			duration: int(song.Track.DurationMs / 1000),
+		}
+	}
+	close(jobs)
+
+	for i := 0; i < len(tracksToDownload); i++ {
+		result := <-results
+		if result.err != nil {
+			fmt.Printf("Error with %s by %s: %s\n", result.track.title, result.track.artist, result.err)
+		} else {
+			fmt.Printf("Finished %s by %s\n", result.track.title, result.track.artist)
+		}
+	}
+	return
+}
+
+func getSpotifyPlaylist(bearerToken, playlistID string) (spotifyJSON Spotify, err error) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("https://api.spotify.com/v1/playlists/%s/tracks", playlistID), nil)
+	if err != nil {
+		return
+	}
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+bearerToken)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	bodyBytes, _ := ioutil.ReadAll(resp.Body)
+	err = json.Unmarshal(bodyBytes, &spotifyJSON)
+	return
+}
--- spotifydownload/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""Top-level package for Download Spotify playlists."""
-
-__author__ = """Zack Scholl"""
-__email__ = 'zack.scholl@gmail.com'
-__version__ = '0.1.0'
